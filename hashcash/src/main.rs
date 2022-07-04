c3da9a16e3968ad09edfddd0c6e1d639f94a0d70
aragant <tlemaire1610@gmail.com>
2022-07-04 14:27:37 +0200

    computeComplexity

--- hashcash/src/main.rs
+++ hashcash/src/main.rs
@@ -37,7 +37,7 @@ fn hashcash(input: MD5HashCashInput) -> MD5HashCashOutput {
         let mut seed: u64 = random();
         let mut seed_with_message = format!("{}{}", seed.to_string(), input.message);
         let mut hashcode128 = u128::from_be_bytes(md5::compute(seed_with_message).0);
-        let current_complexity = hashcode128.leading_zeros();
+        let current_complexity = computeComplexity(hashcode128);
         let hashcode = format!("{:x}", hashcode128);
         if current_complexity >= input.complexity{
             return MD5HashCashOutput {
@@ -46,4 +46,9 @@ fn hashcash(input: MD5HashCashInput) -> MD5HashCashOutput {
             }
         }
     }
+}
+
+fn computeComplexity(hashcode: u128) -> u32 {
+    let hashcode128 = u128::from_be_bytes(md5::compute(hashcode).0);
+    hashcode128.leading_zeros()
 }

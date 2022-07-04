870dad45be5ad522d81aff7f44562b3b568b7622
aragant <tlemaire1610@gmail.com>
2022-07-04 15:33:37 +0200

    splice md5 in function

--- hashcash/src/main.rs
+++ hashcash/src/main.rs
@@ -36,7 +36,7 @@ fn hashcash(input: MD5HashCashInput) -> MD5HashCashOutput {
     {
         let mut seed: u64 = random();
         let mut seed_with_message = format!("{}{}", seed.to_string(), input.message);
-        let mut hashcode128 = u128::from_be_bytes(md5::compute(seed_with_message).0);
+        let mut hashcode128 = computeMd5ToU128(seed_with_message);
         let current_complexity = computeComplexity(hashcode128);
         let hashcode = format!("{:x}", hashcode128);
         if current_complexity >= input.complexity{
@@ -49,6 +49,9 @@ fn hashcash(input: MD5HashCashInput) -> MD5HashCashOutput {
 }
 
 fn computeComplexity(hashcode: u128) -> u32 {
-    let hashcode128 = u128::from_be_bytes(md5::compute(hashcode).0);
-    hashcode128.leading_zeros()
+    hashcode.leading_zeros()
+}
+
+fn computeMd5ToU128(message: String) -> u128 {
+    u128::from_be_bytes(md5::compute(message).0)
 }
